0d2755f3cd4b975732ece614b905ac3af699881e
reydoriz-lab <reydoriz@gmail.com>
2026-06-10 23:02:12 +0500

    fix: use env variables

--- config.js
+++ config.js
@@ -1,17 +1,8 @@
 module.exports = {
-    // Токен бота и ID группы
-    token: 'vk1.a.LZYru_KjcqQj5rJtoAr_FqYVVu54Jj6NNQ2mKA6fhmqYJAFfZUVN0tSk0rVhrbuKjub4MPT_Dv55IvHG3uTc7cVD0uxeyCOZuIj1pZk4UtbnssLGmcZOo8_IdmFreNKbF8wD3i90yewsScag3eckEfdDhy_NcPU4RF-R6iNS6jtsA7UUShh1rFdgDTf-O-3QsYpmQqQMWPH46doW6hMW9A',
-    groupId: 238471646,
-    
-    // Твой админ ID
-    adminId: 286888243,
-    
-    // Настройки поиска
+    token: process.env.token,
+    groupId: parseInt(process.env.groupId),
+    adminId: parseInt(process.env.adminId),
     ageRange: 4,
-    
-    // Папка для временных фото
     tempFolder: './temp',
-    
-    // Версия API
     apiVersion: '5.199'
 };
